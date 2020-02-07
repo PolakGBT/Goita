200215c75a7aee9d5b9e50f1a854f18d2d46152e
unknown <gizwwo@gmail.com>
2020-02-07 14:24:36 +0100

    contact-network

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,54 +1,60 @@
 import React from "react";
 import "./CSS/contact.css"
+import FB from  "../images/facebook.png"
+import INSTA from  "../images/instagram-sketched.png"
 const Contact = () => {
     return (
         <>
-            <div style={{
-                width:'100%',
-                height:'200px',
-                backgroundColor:'#F1F1F1',
-                display:'flex',
-                margin:'auto',
-                alignItems:'center'
-            }}
-            class="contact-mobile"
-            id="Conntact"
-            >
-                <h1 style={{
-                    color:'#848484',
-                    fontFamily:' Didot',
-                    fontStyle: 'normal',
-                    fontWeight: '500',
-                    margin:'auto',
-                }}>GOITA</h1>
-                <p style={{
-                    fontFamily: 'JaapokkienchanceRegular',
-                    fontSize: '16px',
-                    letterSpacing: '0.5px',
-                    color: 'black',
-                    margin:'auto',
-                }}
-                class="contactme"
-                >Contactez-Moi</p>
-                <div style={{
-                    display:'flex',
-                    margin:'auto',
-                    flexDirection:'column'
-                }}>
+            <div class="contact-mobile" id="Conntact">
+                <div class="three">
+                    <h1 style={{
+                        color:'#848484',
+                        fontFamily:' Didot',
+                        fontStyle: 'normal',
+                        fontWeight: '500',
+                        margin:'auto',
+                    }}>GOITA</h1>
+                </div>
+                <div class="three">
                     <p style={{
                         fontFamily: 'JaapokkienchanceRegular',
                         fontSize: '16px',
                         letterSpacing: '0.5px',
                         color: 'black',
                         margin:'auto',
-                    }}>Email : goitag868@gmail.com</p>
-                    <p style={{
-                        fontFamily: 'JaapokkienchanceRegular',
-                        fontSize: '16px',
-                        letterSpacing: '0.5px',
-                        color: 'black',
+                    }}
+                    class="contactme"
+                    >Contactez-Moi</p>
+                </div>
+                <div class="three">
+                    <div style={{
+                        display:'flex',
                         margin:'auto',
-                    }}>Téléphone : 07 55 22 67 90</p>
+                        flexDirection:'column'
+                    }}>
+                        <p style={{
+                            fontFamily: 'JaapokkienchanceRegular',
+                            fontSize: '16px',
+                            letterSpacing: '0.5px',
+                            color: 'black',
+                            margin:'auto',
+                        }}>Email : goitag868@gmail.com</p>
+                        <p style={{
+                            fontFamily: 'JaapokkienchanceRegular',
+                            fontSize: '16px',
+                            letterSpacing: '0.5px',
+                            color: 'black',
+                            margin:'auto',
+                        }}>Téléphone : 07 55 22 67 90</p>
+                        <div class="contact-network">
+                            <a href="https://www.facebook.com/gaoussou.goita.7">
+                                <img src={FB}></img>
+                            </a>
+                            <a>
+                                <img src={INSTA}></img>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="copy">
